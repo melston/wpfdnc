e8857d56beffbebaa304346ad96447906fb14fbf
Mark Elston <Mark.Elston@advantest.com>
2021-02-25 11:25:19 -0800

    Now supports moving chapters from one Book to another

--- WpfBooks/MainWindowDnD.cs
+++ WpfBooks/MainWindowDnD.cs
@@ -19,14 +19,26 @@ namespace WpfBooks
     /// </summary>
     public partial class MainWindow : Window
     {
-        private Point _lastMouseDown;
-        private Chapter _draggedItem, _target;
+        private Point _selectedMouseDown;
+        private Chapter _selectedChapter, _target;
+        private Book _sourceBook;
+
+        private void authorsTree_ItemSelected(object sender, RoutedEventArgs e)
+        {
+            _selectedChapter = authorsTree.SelectedItem as Chapter;
+            if (_selectedChapter != null)
+            {
+                SourceChapterName.Text = _selectedChapter.ChapterName;
+                _sourceBook = GetNearestBook(e.OriginalSource as TreeViewItem);
+                if (_sourceBook != null) SourceBookName.Text = _sourceBook.Title;
+            }
+        }
 
         private void authorsTree_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
             {
-                _lastMouseDown = e.GetPosition(authorsTree);
+                _selectedMouseDown = e.GetPosition(authorsTree);
             }
         }
 
@@ -38,29 +50,12 @@ namespace WpfBooks
                 {
                     Point currentPosition = e.GetPosition(authorsTree);
 
-                    if ((Math.Abs(currentPosition.X - _lastMouseDown.X) > 10.0) ||
-                        (Math.Abs(currentPosition.Y - _lastMouseDown.Y) > 10.0))
+                    if (MovedEnough(currentPosition, _selectedMouseDown) && _selectedChapter != null)
                     {
-                        _draggedItem = authorsTree.SelectedItem as Chapter;
-                        if (_draggedItem != null)
-                        {
-                            //Console.WriteLine("  _draggedItem != null");
-                            DragDropEffects finalDropEffect =
-                                DragDrop.DoDragDrop(authorsTree, authorsTree.SelectedValue,
-                                                    DragDropEffects.Move);
-
-                            //Checking target is not null and item is dragging(moving)
-                            if ((finalDropEffect == DragDropEffects.Move) && (_target != null))
-                            {
-                                // A Move drop was accepted
-                                //if (!_draggedItem.Header.ToString().Equals(_target.Header.ToString()))
-                                //{
-                                //    CopyItem(_draggedItem, _target);
-                                //    _target = null;
-                                //    _draggedItem = null;
-                                //}
-                            }
-                        }
+                        DropEffectText.Text = "MouseMove Effects=Move";
+                        DragDropEffects finalDropEffect =
+                            DragDrop.DoDragDrop(authorsTree, authorsTree.SelectedValue,
+                                                DragDropEffects.Move);
                     }
                 }
             }
@@ -74,17 +69,18 @@ namespace WpfBooks
             {
                 Point currentPosition = e.GetPosition(authorsTree);
 
-                if ((Math.Abs(currentPosition.X - _lastMouseDown.X) > 10.0) ||
-                   (Math.Abs(currentPosition.Y - _lastMouseDown.Y) > 10.0))
+                if (MovedEnough(currentPosition, _selectedMouseDown))
                 {
                     // Verify that this is a valid drop and then store the drop target
-                    Chapter item = GetNearestChapter(e.OriginalSource as UIElement);
-                    if (item != null && CheckDropTarget(_draggedItem, item))
+                    Chapter chapTarget = GetNearestChapter(e.OriginalSource as UIElement);
+                    if (chapTarget != null && CheckDropTarget(_selectedChapter, chapTarget))
                     {
                         e.Effects = DragDropEffects.Move;
+                        DropEffectText.Text = "DragOver Effects=Move, Target=" + chapTarget.ChapterName;
                     }
                     else
                     {
+                        DropEffectText.Text = "DragOver Effects=None";
                         e.Effects = DragDropEffects.None;
                     }
                 }
@@ -104,10 +100,15 @@ namespace WpfBooks
 
                 // Verify that this is a valid drop and then store the drop target
                 Chapter TargetItem = GetNearestChapter(e.OriginalSource as UIElement);
-                if (TargetItem != null && _draggedItem != null)
+                Book TargetBook = GetNearestBook(e.OriginalSource as UIElement);
+                if (TargetItem != null && TargetBook != null && _selectedChapter != null)
                 {
                     _target = TargetItem;
                     e.Effects = DragDropEffects.Move;
+                    DropEffectText.Text = "Drop Effects=Move, TargetBook=" + TargetBook.Title +
+                        ", TargetChapter=" + TargetItem.ChapterName;
+                    int moveIdx = TargetBook.Chapters.IndexOf(TargetItem);
+                    MoveChapter(_selectedChapter, _sourceBook, TargetBook, moveIdx);
                 }
             }
             catch (Exception)
@@ -115,121 +116,54 @@ namespace WpfBooks
             }
         }
 
+        private bool MovedEnough(Point currPoint, Point origPoint)
+        {
+            if ((Math.Abs(currPoint.X - origPoint.X) > 10.0) ||
+                   (Math.Abs(currPoint.Y - origPoint.Y) > 10.0))
+            {
+                return true;
+            }
+            return false;
+        }
+
         private bool CheckDropTarget(Chapter _sourceItem, Chapter _targetItem)
         {
             //Check whether the target item is meeting your condition
-            bool _isEqual = false;
+            bool dropOK = false;
             if (!_sourceItem.ChapterName.Equals(_targetItem.ChapterName))
             {
-                _isEqual = true;
+                dropOK = true;
             }
-            return _isEqual;
+            return dropOK;
 
-        }
-
-        private T GetNearest<T>(UIElement element)
-        {
-            // Walk up the element tree to the nearest tree view item.
-            TreeViewItem container = element as TreeViewItem;
-            T chap = default(T);
-            while ((container == null) && (element != null))
-            {
-                element = VisualTreeHelper.GetParent(element) as UIElement;
-                container = element as TreeViewItem;
-            }
-
-            try
-            {
-                chap = (T)container.Header;
-            }
-            catch (Exception) { }
-            return chap;
         }
 
         private Chapter GetNearestChapter(UIElement element)
         {
             // Walk up the element tree to the nearest tree view item.
             TreeViewItem container = element as TreeViewItem;
-            Chapter chap = null;
             while ((container == null) && (element != null))
             {
                 element = VisualTreeHelper.GetParent(element) as UIElement;
                 container = element as TreeViewItem;
             }
 
-            try
-            {
-                chap = container.Header as Chapter;
-            }
-            catch (Exception) { }
-            return chap;
+            return container.Header as Chapter;
         }
 
-        //private void CopyItem(TreeViewItem _sourceItem, TreeViewItem _targetItem)
-        //{
-        //    //Asking user wether he want to drop the dragged TreeViewItem here or not
-        //    if (MessageBox.Show("Would you like to drop " + _sourceItem.Header.ToString() + " into " + _targetItem.Header.ToString() + "", "", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
-        //    {
-        //        try
-        //        {
-        //            //adding dragged TreeViewItem in target TreeViewItem
-        //            AddChild(_sourceItem, _targetItem);
+        private Book GetNearestBook(UIElement element)
+        {
+            // Walk up the element tree to the nearest tree view item that contains a book
+            TreeViewItem container = element as TreeViewItem;
+            Book bk = null;
+            while ((bk == null) && (element != null))
+            {
+                element = VisualTreeHelper.GetParent(element) as UIElement;
+                container = element as TreeViewItem;
+                if (container != null) bk = container.Header as Book;
+            }
 
-        //            //finding Parent TreeViewItem of dragged TreeViewItem 
-        //            TreeViewItem ParentItem = FindVisualParent<TreeViewItem>(_sourceItem);
-        //            // if parent is null then remove from TreeView else remove from Parent TreeViewItem
-        //            if (ParentItem == null)
-        //            {
-        //                authorsTree.Items.Remove(_sourceItem);
-        //            }
-        //            else
-        //            {
-        //                ParentItem.Items.Remove(_sourceItem);
-        //            }
-        //        }
-        //        catch
-        //        {
-
-        //        }
-        //    }
-
-        //}
-
-        //public void AddChild(TreeViewItem _sourceItem, TreeViewItem _targetItem)
-        //{
-        //    // add item in target TreeViewItem 
-        //    TreeViewItem item1 = new TreeViewItem();
-        //    item1.Header = _sourceItem.Header;
-        //    _targetItem.Items.Add(item1);
-        //    foreach (TreeViewItem item in _sourceItem.Items)
-        //    {
-        //        AddChild(item, item1);
-        //    }
-        //}
-
-        //static TObject FindVisualParent<TObject>(UIElement child) where TObject : UIElement
-        //{
-        //    if (child == null)
-        //    {
-        //        return null;
-        //    }
-
-        //    UIElement parent = VisualTreeHelper.GetParent(child) as UIElement;
-
-        //    while (parent != null)
-        //    {
-        //        TObject found = parent as TObject;
-        //        if (found != null)
-        //        {
-        //            return found;
-        //        }
-        //        else
-        //        {
-        //            parent = VisualTreeHelper.GetParent(parent) as UIElement;
-        //        }
-        //    }
-
-        //    return null;
-        //}
+            return bk;
+        }
     }
 }
